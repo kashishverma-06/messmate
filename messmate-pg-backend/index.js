951c3abdebc9eb567b483ca17677d269ed64f1fc
kashishverma-06 <kashishverma1806@gmail.com>
2026-06-29 18:04:36 +0530

    Initail commit

---
+++ messmate-pg-backend/index.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const app = express();
+const cors = require("cors");
+require('dotenv').config();
+
+console.log(process.env.JWT_SECRET);
+
+const PORT = process.env.PORT || 4000;
+
+const sequelize = require('./config/db');
+const Mess = require('./models/Mess');
+const profileRoutes = require('./routes/auth');
+
+app.use(express.json());
+app.use(cors());
+
+const messRoutes = require('./routes/messRoutes');
+const authRoutes = require('./routes/auth');
+app.use('/messes', messRoutes);
+app.use('/api/auth', authRoutes);
+//app.use('/profile', profileRoutes);
+
+sequelize.authenticate()
+  .then(() => {
+    console.log('✅ PostgreSQL connected');
+    return sequelize.sync();
+  })
+  .then(() => {
+    console.log('✅ Models synced to database');
+    app.listen(PORT, () => {
+      console.log(`Server running at http://localhost:${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.error('❌ DB error:', err.message);
+  });
