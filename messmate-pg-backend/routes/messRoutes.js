de08f33fd48e47a95c17eb515ffb198a45b7f46c
kashishverma-06 <kashishverma1806@gmail.com>
2026-06-30 21:54:39 +0530

    Fix model import paths

--- messmate-pg-backend/routes/messRoutes.js
+++ messmate-pg-backend/routes/messRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const Mess = require('../models/Mess');
+const Mess = require('../models/mess');
 const { Op } = require('sequelize');
 
 // GET all messes
